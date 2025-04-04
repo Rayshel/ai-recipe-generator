d205d835b150dc1f8929dddea6eced2c3c788d6b
Rachael Ijeoma <ijeomarachel01@gmail.com>
2025-04-04 19:59:30 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -8,3 +8,23 @@ createRoot(document.getElementById('root')).render(
     <App />
   </StrictMode>,
 )
+import { Amplify } from 'aws-amplify';
+import { Authenticator } from '@aws-amplify/ui-react';
+import awsExports from './aws-exports';
+
+Amplify.configure(awsExports);
+
+function App() {
+  return (
+    <Authenticator>
+      {({ signOut, user }) => (
+        <main>
+          <h1>Hello {user.username}</h1>
+          <button onClick={signOut}>Sign out</button>
+        </main>
+      )}
+    </Authenticator>
+  );
+}
+
+export default App;
